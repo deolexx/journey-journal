dd84692a60aa3ab7ac112131e61af1d85356cfa0
s.lukianenko <lukianenko88@gmail.com>
2024-04-14 23:30:45 +0300

    initial commit

---
+++ src/main/java/com/deo/journeyjournal/JourneyJournalApplication.java
@@ -0,0 +1,13 @@
+package com.deo.journeyjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyJournalApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(JourneyJournalApplication.class, args);
+  }
+}
---
+++ src/test/java/com/deo/journeyjournal/JourneyJournalApplicationTests.java
@@ -0,0 +1,11 @@
+package com.deo.journeyjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyJournalApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
